d42de9f80bdc7e395f893c58d531eb5d7471075a
wangjian <wangjian0504@gmail.com>
2022-09-22 22:36:06 +0800

    log服务(未完成)

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+	"logger-service/data"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoUrl = "mongodb://mongo:27017"
+	gRpcProt = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+	Models data.Models
+}
+
+func main() {
+	// connect to mongo
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+
+	// create a context in order to disconnect
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	app := Config{
+		Models: data.New(client),
+	}
+
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI(mongoUrl)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("error connect To Mongo")
+		return nil, err
+	}
+	return c, nil
+}
